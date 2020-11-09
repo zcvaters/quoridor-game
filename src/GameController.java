b2c4fca7268bd700c8a682413f2d376a45d45720
n33omc <n33omc@mun.ca>
2020-11-09 11:37:23 -0330

    Updated GameController.  Adding variable to track next player's turn.

--- src/GameController.java
+++ src/GameController.java
@@ -1,34 +1,33 @@
 
 public class GameController {
 	
+	//cache ref to players and turn info
+	Player[] allPlayers;
+	//index for keeping track of turns for players (0-3)
+	int nextPlayerIndex;
+	Player currentPlayer;
+	
+	//store reference to collection of tiles
+	GameTile[][] tiles;
+	
 	//the GameState which will store a snapshot of a game in progress
 	GameState gameState;
 	
-	/* Constructor - GameController (the manager responsible for general game play.)
-	 * 
-	 * @Params:
-	 * 		
-	 */	
-
-	public GameController(InGameUIPanel inGameUIPanel, GameBoard gameBoard, Player[] players, int nextTurn) {
-		//debugging, delete this stuff
-		GameTile[][] tiles = gameBoard.GetGrid();
+	//constructor
+	public GameController(InGameUIPanel inGameUIPanel, GameBoard gameBoard, Player[] players, int nextToPlay) {
 		
-		System.out.println("Board is " +tiles[0].length+ "x" +tiles[1].length+ "\n");
-		for(int i = 0; i < players.length; i++) {
-			System.out.println("Name: "+ players[i].GetName()+ 
-							   "\nType: " +players[i].GetType()+
-							   "\nColor: " +players[i].GetColor()+
-							   "\nOrder: " +players[i].GetTurnPosition()+
-							   "\nGameTile: (" +(players[i].GetTile().GetXCoord()+1) + 
-							   				", "+(players[i].GetTile().GetYCoord()+1) +  ")\n");
-			
-		}
-		System.out.println("Next Turn is " +nextTurn);
-		System.out.println("Next Player is " +players[nextTurn].GetName());
-		//
-		//
+		//copy ref to GameSettings
+		GameSettings.SetGameController(this);
+		this.allPlayers = players;
+		//For a new game, nextPlayer = 0.  Then 1, 2, 3, 0, 1, 2, 3...
+		this.nextPlayerIndex = nextToPlay;
+		this.currentPlayer = allPlayers[nextPlayerIndex];
 		
+		//cache the tiles
+		this.tiles = gameBoard.GetGrid();
+		
+		
+						
 		//put the players in position on the board
 		for(Player thisPlayer : players) {
 			//get the assigned starting tile
@@ -39,7 +38,100 @@ public class GameController {
 		
 		
 		//show the gameboard!
-		GameSettings.GetMainWindow().ShowPanel(inGameUIPanel);			
+		GameSettings.GetMainWindow().ShowPanel(inGameUIPanel);
+		//advance to next turn, pass player list index as ref.
+		//AdvanceToNextTurn();
 	}
+	
+	//GETTERS
+	public Player GetCurrentPlayer() {
+		//return the player taking the current turn
+		return currentPlayer;
+	}
+	
+	/*
+	public void AdvanceToNextTurn() {
+		
+		//get the next player to play
+		Player nextPlayer = allPlayers[nextPlayerIndex];				
+		System.out.println("Advancing to next turn. Next player is " +nextPlayer.GetName());
+		
+		//loop through the tiles, deactivate all highlights
+		for(int x = 0; x < GameSettings.GetRows(); x++) {
+			for(int y = 0; y < GameSettings.GetCols(); y++) {				
+				tiles[x][y].DeactivateTile();				
+			}
+		}
+		
+		//pause the game, lock the controls (activated when player clicks on message)
+		GameSettings.SetGameIsPaused(true);
+		System.out.println("game paused? " +GameSettings.GetGameIsPaused());
+		GameSettings.GetMainWindow().ShowMessage(nextPlayer.GetName() + ", it's your turn!");
+		currentPlayer = nextPlayer;	
+		
+		//increment the nextPlayerIndex, so the turns will advance through different players
+		nextPlayerIndex++;
+		//if past end, loop back to start
+		if(nextPlayerIndex >3) {
+			nextPlayerIndex = 0;
+		}
+	}
+	
+	public void BeginTurn() {
+		//remove the popup notification
+		GameSettings.GetMainWindow().RemoveMessage();
+		//allow tracking of input
+		GameSettings.SetGameIsPaused(false);		
+		System.out.println("game paused? " +GameSettings.GetGameIsPaused());
+		System.out.println("Starting turn for " +currentPlayer.GetName());
+		//it is now "currentPlayer"'s turn
+		ActivateLegalTiles(currentPlayer);
+		
+		
+	}
+	
+	private void ActivateLegalTiles(Player currentPlayer) {
+		
+		//move this to GameBoard class?
+		
+		//get the 2D array of gametiles
+		GameTile[][] tiles = GameSettings.getGameTiles();
+		
+		//get the number of rows and cols on gameboard
+		//we need this zero indexed, so subtract one.
+		int rows = GameSettings.GetRows() - 1; 
+		int cols = GameSettings.GetCols() - 1;
+		//get the gameTile that the player is standing on		
+		GameTile location = currentPlayer.GetTile();
+		//get the x, y, address of this tile
+		int rowAddress = location.GetXCoord();
+		int colAddress = location.GetYCoord();		
+		
+		System.out.println(currentPlayer.GetName() + " is on tile (" +rowAddress+ ", " +colAddress+ ")");
+		
+		//does player have north?
+		if(location.HasNorth()) {
+			tiles[rowAddress-1][colAddress].ActivateTile();
+		}
+		//does player have east?
+		if(location.HasEast()) {
+			tiles[rowAddress][colAddress+1].ActivateTile();
+		}
+		//does player have south?
+		if(location.HasSouth()) {
+			tiles[rowAddress+1][colAddress].ActivateTile();
+		}
+		//does player have west?
+		if(location.HasWest()) {
+			tiles[rowAddress][colAddress-1].ActivateTile();
+		}
+		
+		
+		
+	}
+	
+	*/
+	
+	
 
 }
