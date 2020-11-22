210cab419757df889dab8213d0ce62bd8cc8ebce
zcvaters <zachvaters@gmail.com>
2020-11-22 12:20:53 -0330

    - refactor: Tidy up player info display UI, Tidy up instructions panel

--- src/InstructionsMenu.java
+++ src/InstructionsMenu.java
@@ -70,16 +70,16 @@ public class InstructionsMenu extends JPanel implements ActionListener {
 		EmptyBorder border2 = new EmptyBorder(50, 15, 0, 15);
 		instructionsPanel.setOpaque(false);
 		instructionsPanel.setBorder(border2);
-		//JTextArea instructionsText = new JTextArea();
-		//instructionsText.setFont(new Font("Dialog", Font.PLAIN, 14));
-		//instructionsText.setText(GetInstructionsText());
 		JScrollPane instructionsScrollPane = new JScrollPane(editorPane, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 		JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		instructionsScrollPane.setOpaque(false);
 		instructionsScrollPane.setPreferredSize(new Dimension(900, 500));
-		editorPane.setPreferredSize(new Dimension(900, 500));
+		editorPane.setPreferredSize(new Dimension(900, 600));
 		editorPane.setOpaque(false);
-		instructionsPanel.add(editorPane);
+		instructionsScrollPane.setOpaque(false);
+		//instructionsScrollPane.add(editorPane);
+		instructionsPanel.add(instructionsScrollPane);
+		instructionsPanel.setOpaque(false);
 
 		// the "back" button and listener
 		JPanel buttonPanel = new JPanel();
@@ -100,29 +100,6 @@ public class InstructionsMenu extends JPanel implements ActionListener {
 		this.add(instructionsPanel);
 		this.add(buttonPanel);
 		this.setVisible(false);
-	}
-
-	private String GetInstructionsText() {
-		String text = "\nStarting a New Game\n\n"
-				+ "Begin by starting a New Game, choose your colour set. Each colour set modifies the board colour and each pawn colour.\n"
-				+ "You can also choose each players name and if they're a human player or if they're are a computer AI.\n"
-				+ "There must be four players to begin. Each Computer AI can be set to Easy or Hard difficulty. \n"
-				+ "Click Let's Play when you're all set up to begin play!\n\n" + "Start Of Play\n\n"
-				+ "Each player starts at one side of the board, each player is given 5 fences. \n"
-				+ "Every player has a unique pawn and a different goal to win the game. \n"
-				+ "The players goal is to reach the opposite side they started on. First to get there wins!\n"
-				+ "The starting pawn is determined randomly each session.\n"
-				+ "At the start of the match the board is empty except for the starting positions of each pawn.\n\n"
-				+ "Taking A Turn\n\n"
-				+ "A pawn can move one square at a time, horizontally or vertically, forwards or backwards, never diagonally.\n"
-				+ "Fences block a players path, Fences never move once they're placed. Fences take up two total squares.\n"
-				+ "Face to Face pawns can jump over eachother or go in any other direction they choose.\n"
-				+ "Face to Face pawns may also go diagonal if there is a fence behind their opponent.\n"
-				+ "Face to Face pawns may not jump over more than one pawn at a time.\n"
-				+ "Fences are strictly impossible to jump over.\n\n" + "End of the Game\n\n"
-				+ "When a player reaches the opposite side of where they began the game is over.\n"
-				+ "The first player to do so is the Winner!\n\n";
-		return text;
 	}
 
 
